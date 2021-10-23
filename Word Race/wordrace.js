abb775d12117e574b2256ddfb80da6463e4e60ce
pedroheck <pedroscheck@hotmail.com>
2021-10-23 12:09:48 -0300

    Creating next generation and adding mutation

--- Word Race/wordrace.js
+++ Word Race/wordrace.js
@@ -1,9 +1,9 @@
-var characters = 'ABCÇDEFGHIJKLMNOPQRSTUVWXYZabcçdefghijklmnopqrstuvwxyz';
+var characters = 'ABCÇDEFGHIJKLMNOPQRSTUVWXYZ abcçdefghijklmnopqrstuvwxyz';
 
 var population = [];
 var fitness = [];
-var popSize = 200;
-var word = 'paralelepipedo';
+var popSize = 1000;
+var word = 'Paralelepipedo';
 
 function generateInitialPop(){
     for(var i = 0; i < popSize; i++){ // Generates each individual
@@ -18,6 +18,8 @@ function generateInitialPop(){
 }
 
 function calculateFitness(){
+    fitness = [];
+    
     for(var i = 0; i < population.length; i++){ // Runs through each individual
         var individual = population[i];
         var indiv_fitness = 0;
@@ -33,6 +35,63 @@ function calculateFitness(){
     }
 }
 
+
+function createNextGeneration(){
+    var matingPool = []; // [A1, A1, A1, B1, C1, C1, D1, D1, D1, D1, E1, E1, ...] => The bigger the fitness, the bigger the number of appearances
+    var newPopulation = []; // [A1, C1, D1, ...]
+
+    for(var i = 0; i < population.length; i++){ // Populates the matingPool according to their fitness
+        var num = parseInt((fitness[i] * 100).toFixed(0)) + 1; // The + 1 is to give even the least fit a chance to be selected
+        for(var j = 0; j < num; j++){
+            matingPool.push(population[i]);
+        }
+    }
+
+    for(var i = 0; i < population.length; i++){ // Creates the new population
+        var indexA = Math.floor(Math.random() * matingPool.length);
+        var indexB = Math.floor(Math.random() * matingPool.length);
+        var child = crossover(matingPool[indexA], matingPool[indexB]);
+
+        child = mutate(child);
+
+        newPopulation.push(child);
+    }
+
+    population = newPopulation;
+
+    calculateFitness();
+}
+
+function crossover(ind1, ind2){
+    var child = '';
+    for(var i = 0; i < ind1.length; i++){
+        if(Math.random() < 0.5){
+            child += ind1[i];
+        } else {
+            child += ind2[i];
+        }
+    }
+    return child;
+}
+
+function mutate(individual){
+    for(var i = 0; i < individual.length; i++){
+        if(Math.random() < 0.02){ // Gives 2% chance to mutate
+            individual = individual.replace(individual[i], characters[Math.floor(Math.random() * characters.length)]);
+        }
+    }
+    return individual;
+}
+
+function getHighestFitness(){
+    var highest = 0;
+    for(var i = 0; i < fitness.length; i++){
+        if(fitness[i] > highest){
+            highest = fitness[i];
+        }
+    }
+    return highest;
+}
 
 
 generateInitialPop();
